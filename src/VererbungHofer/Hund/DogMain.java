a0b028ea83f5c40bfb0bdee4048d2f55617f51a9
Victoria Egger <125293871+VictoriaEgger@users.noreply.github.com>
2023-05-02 16:03:51 +0200

    02.05.2023

--- src/VererbungHofer/Hund/DogMain.java
+++ src/VererbungHofer/Hund/DogMain.java
@@ -16,7 +16,7 @@ public class DogMain {
 
         //Up-Casting
         //Kann jetzt auf Dog und Beagle zugreifen. Und es wurde nur ein Object erzeugt.
-        Beagle freddie = new Beagle("Franz",2,"Gulasch");
+        Beagle freddie = new Beagle("Franz", 2, "Gulasch");
         Dog doggo = freddie;
 
         //bellt wie ein beagle, weil eine beagle instanz auf der dog referenz ist.
@@ -28,7 +28,39 @@ public class DogMain {
 // Will die methoden von Beagle nutzen um lieblingsessen herauszufinden. Aber Hubert ist auf einer Dog Klasse
         // Deshalb downcasten
 
+
         Beagle b1 = (Beagle) doggo2;
         b1.eatsLovedFood();
+
+        Dog rex = new KommisarRex(20);
+
+        //Instanceof OPerator: Überprüft ob Instanz der klasse entspricht
+
+
+        //schaut ob das objekt doggo2 zur basisklasse hund gehört
+        //Kann der Begale dass was ein hund kann?
+        if (doggo2 instanceof Dog) {
+            System.out.println("jo. is a hund.");
+        }
+
+        //Schaut ob das object doggo2 ein beagle ist
+        if (doggo2 instanceof Beagle) {
+            System.out.println("jo. is a beagle.");
+        }
+
+        //nicht so schön. überprüft intsanz genau einer konkreten klasse entspricht
+        //klasse entspricht(nicht durch abgeleitete instanz ersetzbar)
+        //Bist du Beagle genau ein Hund?
+
+        if (doggo2.getClass().equals(Beagle.class)) {
+            System.out.println("Yay auf doggo2 ist wirklich ein Beagle und kein anderer Hund");
+        }
+
+        //Get nicht weil doggo2 ein Beagle ist und andere Methoden hat. Ist eine Kindklasse Beagle und keinen Basisklasse Hund
+        if (doggo2.getClass().equals(Dog.class)) {
+            System.out.println("Yay auf doggo2 ist wirklich ein Hund und kein anderer Hund");
+        } else {
+            System.out.println("Beagle ist kein Hund - sondern ein Beagle");
+        }
     }
 }
